c777b1fd3c72bcadd848812fb3c958ab4a6f362d
McCall Woodward <woodwardem@g.crookcoountyschools.org>
2021-02-19 09:02:06 -0800

    learned about arrays

---
+++ script/scrpt.js
@@ -0,0 +1,18 @@
+//console.log('hello world')
+//Simple Arrays
+
+let items = ['pencil', 'hat', 'book'];
+console.log(items[0]);
+console.log(items[2]);
+
+console.log(items.length);
+
+
+//how to add items to the array
+items.push('laptop');
+items[4] = 'suitcase';
+console.log(items.length);
+console.log('new item is', items[3]);
+console.log(items.pop());
+
+console.log(items);
